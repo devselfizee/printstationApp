69d34506848893d0cb58d9e2de94fe18f49da8b9
Paul <pauled8250188@gmail.com>
2025-12-09 10:43:11 +0300

    redirection vers panier

--- renderer/src/pages/listing.js
+++ renderer/src/pages/listing.js
@@ -125,9 +125,9 @@ export const renderListing = (root) => {
 
   // Click sur cart-detail pour aller au panier
   footer.querySelector('.cart-detail').onclick = () => {
-    alert("Ce bouton n'est pas encore fonctionnel pour le moment.");
-    // state.page = 'cart';
-    // window.render();
+    // alert("Ce bouton n'est pas encore fonctionnel pour le moment.");
+    state.page = 'detail';
+    window.render();
   };
 
   main.appendChild(footer);
